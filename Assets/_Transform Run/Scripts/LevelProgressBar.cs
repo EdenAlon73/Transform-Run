61ff4237cc2ecfdf2fdf28d268cd18b3a6054565
EdenAlon73 <61311576+EdenAlon73@users.noreply.github.com>
2021-05-05 16:15:21 +0300

    finished progress bar + added sprite editor + added recorder + added finished line

---
+++ Assets/_Transform Run/Scripts/LevelProgressBar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelProgressBar : MonoBehaviour
+{
+    private GameObject finishLineObj;
+    private Slider levelProgressSlider;
+    private float finishLineZValue;
+    private float playerZValue;
+
+
+    private void Awake()
+    {
+        levelProgressSlider = GameObject.Find("Slider_LevelProgress").GetComponent<Slider>();
+        finishLineObj = GameObject.FindWithTag("Finish Line");
+    }
+
+    private void Start()
+    {
+        finishLineZValue = finishLineObj.transform.position.z;
+        levelProgressSlider.minValue = 0;
+        levelProgressSlider.maxValue = finishLineZValue;
+    }
+    
+    private void Update()
+    {
+        CalculateSliderValue();
+    }
+
+    private void CalculateSliderValue()
+    {
+        playerZValue = transform.position.z;
+        levelProgressSlider.value = playerZValue;
+    }
+
+  
+}
