c478e143f80abe3b080b78ccd90f697713fc2848
farkrash <62850026+farkrash@users.noreply.github.com>
2021-05-09 16:07:04 +0300

    Tmp Package, New Jessica Canvas, New Z wall

---
+++ Assets/_Transform Run/Scripts/ZWallLogic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZWallLogic : MonoBehaviour
+{
+    [SerializeField]private GameObject normalWall;
+    [SerializeField]private GameObject crackedWall;
+    private void OnTriggerEnter(Collider other)
+    {
+        normalWall.SetActive(false);
+        crackedWall.SetActive(true);
+    }
+}
