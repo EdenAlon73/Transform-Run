dc623c682b0475bb8add381d91ccae8608049c29
farkrash <62850026+farkrash@users.noreply.github.com>
2021-05-09 14:21:08 +0300

    Repositioned z trail and new zoro player 2 color

--- Assets/_Transform Run/Scripts/StartTrail.cs
+++ Assets/_Transform Run/Scripts/StartTrail.cs
@@ -12,6 +12,11 @@ public class StartTrail : MonoBehaviour
     [SerializeField] private Animator canvasAnim;
     [SerializeField] private GameObject camera1;
     [SerializeField] private GameObject camera2;
+    [SerializeField] private GameObject z;
+    [SerializeField] private GameObject zUpper;
+    [SerializeField] private GameObject zLower;
+    [SerializeField] private TrailRenderer trail;
+    [SerializeField] private SphereCollider playerColl;
     private void Awake()
     {
         playerMover = FindObjectOfType<PlayerMover>();
@@ -25,11 +30,25 @@ public class StartTrail : MonoBehaviour
         zCutOutTop.enabled = true;
         zCutOutBot.enabled = true;
         Invoke("CanvsFadeInAnim", 1f);
+        Invoke("TrailFade", 0.5f);
+        Invoke("CameraSwitch" , 0.3f);
     }
     
     private void CanvsFadeInAnim()
     {
         canvasAnim.SetBool("FadeIn", true);
+        z.SetActive(false);
+        zUpper.SetActive(false);
+        zLower.SetActive(false);
+        playerColl.enabled = false;
+    }
+    private void TrailFade()
+    {
+       
+        trail.enabled = false;
+    }
+    private void CameraSwitch()
+    {
         camera2.SetActive(false);
         camera1.SetActive(true);
     }
