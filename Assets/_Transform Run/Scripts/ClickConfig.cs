542a11f88177da4b42eb360ab2b1296848884b6a
farkrash <62850026+farkrash@users.noreply.github.com>
2021-05-19 15:09:42 +0300

    Build Push

---
+++ Assets/_Transform Run/Scripts/ClickConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tabtale.TTPlugins;
+
+public class ClickConfig : MonoBehaviour
+{
+    private void Awake()
+    {
+        TTPCore.Setup();
+    }
+}
